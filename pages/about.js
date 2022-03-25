8db16e71ba4e3fed6f195fc4b3282d440063ea00
Jaslan Rooney <jaslan.rune.dev@gmail.com>
2022-03-25 09:20:22 -0600

    initial

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+
+const About = () => {
+    return (
+        <>
+         <Head>
+            <title>Ninja List | About</title>
+            <meta name="keywords" content="ninjas" />
+         </Head>
+         <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi quod alias, corrupti velit suscipit id molestias quia illum facilis optio iure non pariatur odio commodi distinctio eos reprehenderit fuga architecto.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi quod alias, corrupti velit suscipit id molestias quia illum facilis optio iure non pariatur odio commodi distinctio eos reprehenderit fuga architecto.</p>
+         </div>
+        </>
+    );
+}
+ 
+export default About;
